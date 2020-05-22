3320d2178b71482806ac88cbc8a4270c9e61ba2a
unknown <unknown@example.com>
2020-05-22 19:13:21 +0800

    Initial Commit

---
+++ pages/locate/locate.js
@@ -0,0 +1,23 @@
+Page({
+
+  data:{
+
+  },
+  getLocation:function(){
+    var that = this;
+    wx.getLocation({
+      success: function(res) {
+        that.setData({
+          lat:res.latitude,
+          lon:res.longitude,
+          speed:res.speed,
+          accuracy:res.accuracy
+        })
+      },
+    })
+  },
+
+  onLoad: function(options) {
+
+  },
+})
